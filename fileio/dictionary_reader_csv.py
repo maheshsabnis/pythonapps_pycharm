bdf49b2632ef496e9168a43fb632763fc5a1dc74
mahesh <sabnis_m@hotmail.com>
2023-09-13 17:12:58 +0530

    Initial Code

--- fileio/dictionary_reader_csv.py
+++ fileio/dictionary_reader_csv.py
@@ -4,4 +4,16 @@
 # the values of the first line.
 
 import csv
+sum_of_salary = 0
+tax = 0
+with open('./../files/data.csv') as f:
+    csv_data = csv.DictReader(f, delimiter='\t')
+    # skip the header
+    next(csv_data)
+    # Show the data
+    print("id \t name\t address \t city\t income \t tax")
+    for data in csv_data:
+        print(f" {data['id']} {data['name']} {data['address']} {data['city']} \t \t {data['income']} {float(data['income']) * 0.2}")
+        sum_of_salary+= float(data['income'])
 
+    print(f"Sum of Salary = {sum_of_salary}")
